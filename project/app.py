d7ef60b2fe10d2cd6e0ccf426aaae08b6f062fcd
Quaj <lychee6to7@gmail.com>
2025-03-18 03:34:04 +0900

    e

--- project/app.py
+++ project/app.py
@@ -1,11 +1,13 @@
 from flask import *
 from user_auth.Members import Members
+from flask_cors import *
 class App:
     __app :Flask
 
     def __init__(self, debugMode):
         print("app instance generate.")
         self.__app = Flask(__name__)
+        CORS(self.__app)
         self.__app.debug = debugMode
 
     def run(self):
