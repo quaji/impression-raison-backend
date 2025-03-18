6f97784f9cb5f6a7d706ca7f8450efd02d3ad8fc
Quaj <lychee6to7@gmail.com>
2025-03-18 17:46:20 +0900

    これじゃねｗ

--- project/app.py
+++ project/app.py
@@ -3,12 +3,16 @@ from project.user_auth.Members import Members
 from flask_cors import *
 class App:
     __app :Flask
+    __members :Members
 
     def __init__(self, debugMode):
         print("app instance generate.")
         self.__app = Flask(__name__)
         CORS(self.__app,origins=["https://your-frontend-app.azurewebsites.net"])
         self.__app.debug = debugMode
+
+    def __wake_up(self):
+        self.__members = Members()
 
     def run(self):
         print("run method in app instance start")
