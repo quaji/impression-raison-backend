e8efcad970f6ebcd2a93076589f7977d131d3ffa
Quaj <lychee6to7@gmail.com>
2025-03-20 02:23:58 +0900

    なんとかなれー

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 from project.app import App
 
-if __name__ == "__main__":
-# def app():
-    appInstance = App(True)
-    app = appInstance.get_app()
+appInstance = App(True)
+app = appInstance.get_app()
