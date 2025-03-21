69d9fc6dc6ba35b6d4f1369600a8c57726b54c2f
Quaj <lychee6to7@gmail.com>
2025-03-22 01:17:28 +0900

    eh1

--- project/user_auth/Members.py
+++ project/user_auth/Members.py
@@ -43,12 +43,12 @@ class Members:
                 self.cursor.execute(SQLquery, (email,))
                 existingData = self.cursor.fetchone()
                 if existingData:
-                    session["email"] = existingData.email
+                    # session["email"] = existingData.email
                     return jsonify({'link':'https://lemon-water-022469c10.6.azurestaticapps.net/signin'}),200
                 else:
                     return jsonify({'link':'https://lemon-water-022469c10.6.azurestaticapps.net/signup'}),200
             except Exception as e:
-                return jsonify({'message': f'Error occurred: {str(e)}'}), 1001
+                return jsonify({'message': f'Error occurred: {str(e)}'}), 500
 
 
 
